83d47b336bf041b98f9472cba889cccced909055
Jose Santos <jsmpereira@gmail.com>
2013-04-24 14:04:58 +0100

    Shell script for starting swank server - for remote access.

---
+++ run-daemon.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+# Create a swank server for now. Actual daemonize process will require more hacking.
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <port>"
+    exit 1
+fi
+
+sbcl --noinform --eval "(require :swank)" \
+     --eval "(swank:create-server :port (parse-integer \"$1\") :style :spawn :dont-close t)"
