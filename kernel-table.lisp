05975a7ba35b78d2592ff64373ceba4076d11658
Jose Santos <jsmpereira@gmail.com>
2013-05-13 19:17:40 +0100

    Kernel table updating: tweaks to foreign functions.

--- kernel-table.lisp
+++ kernel-table.lisp
@@ -9,11 +9,12 @@
 
 (cffi:use-foreign-library libioct)
 
-(defun modify-route (gw dst mask op)
-  (cffi:foreign-funcall "modify_route" :string gw :string dst :string mask :string op))
+(defun modify-route (gw dst op)
+  "Call to foreign function modify_route. Full netmask is used since we're adding HOST routes."
+  (cffi:foreign-funcall "modify_route" :string gw :string dst :string "255.255.255.255" :string op))
 
-(defun add-route (gw dst mask)
-  (modify-route gw dst mask "SIOCADDRT"))
+(defun add-route (gw dst)
+  (modify-route gw dst "SIOCADDRT"))
 
 (defun del-route (gw dst mask)
-  (modify-route gw dst mask "SIOCDELRT"))
+  (modify-route gw dst "SIOCDELRT"))
