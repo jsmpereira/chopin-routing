5ceead652db248d77a9b5d13783209230721b119
Jose Santos <jsmpereira@gmail.com>
2013-05-27 15:35:34 +0100

    Add script for CHOPING-ROUTING setup. Update .gitignore and Makefile.

---
+++ utils/setup.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Jose Santos Martins Pereira jsmpereira@gmail.com
+#
+# Script for setting up CHOPIN-ROUTING.
+#
+# - Fetch routing protocol source
+# - Generate .config file for protocol
+# - Build shared libs (Netlink)
+# - Setup iptable rules for topology building
+
+PROJECT_REPO=https://github.com/jsmpereira/chopin-routing.git
+
+function usage(){
+    echo "Usage: $0 <iface>"
+    exit 1;
+}
+
+function add_mac(){
+    echo "Enter MAC Address:"
+    read MAC
+    echo "---> Setting up iptables rule ..."
+    sudo iptables -t mangle -A PREROUTING -m mac --mac-source $MAC -j ACCEPT;
+    echo "Add another?"
+    select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) add_mac; break;;
+        No ) break;;
+    esac
+    done
+}
+
+function setup_iptables() {
+    echo "---> Setup iptables rules"
+    echo "White list MAC addresses"
+    add_mac
+    echo "---> Dropping packets from everyone else on '$IFACE' ..."
+    sudo iptables -t mangle -A PREROUTING -i $IFACE -j DROP;
+}
+
+function update_source(){
+    echo "---> Fecthing updated source from Github ..."
+    git pull $PROJECT_REPO
+}
+
+function ip_address() {
+    ifconfig $IFACE | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' # IP address
+}
+function broadcast_address() {
+    ifconfig $IFACE | grep "inet addr" | awk -F: '{print $3}' | awk '{print $1}' # Bcast address
+}
+
+function build_config_template() {
+    echo "---> Retrieving Inet and Bcast Address on '$IFACE' ..."
+    inet_addr=$(ip_address)
+    bcast_addr=$(broadcast_address)
+    echo " ... inet: $inet_addr | bcast: $bcast_addr"
+    echo "Generating .config file ..."
+    echo "(:interface \"$IFACE\"
+ :host-address \"$inet_addr\"
+ :broadcast-address \"$bcast_addr\"
+ :port 1234
+ :hop-limit 255
+ :refresh-interval 5
+ :dup-hold-time 30
+ :neighb-hold-time 3
+ :timer-repeat-interval 10)" > ../.config
+    echo "Done."
+}
+
+function build_shared_libs(){
+    echo "---> Building shared libraries ... "
+    cd ../c && make clean && make
+    cd ../
+}
+
+# begin
+[[ $# -eq 0 ]] && usage
+
+IFACE=$1
+
+select option in "Update Source" "Build .config file" "Build Shared Libs" "Setup Topology" "Quit"; do
+case $option in
+    "Update Source" ) update_source;;
+    "Build .config file" ) build_config_template;;
+    "Build Shared Libs" ) build_shared_libs;;
+    "Setup Topology" ) setup_iptables;;
+    "Quit" ) break;;
+    *) echo "Invalid option.";;
+esac
+done
+
+echo "All done."
