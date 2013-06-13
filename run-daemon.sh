be7c96c7e717344bca0398fc08354c38539e6c85
Jose Santos <jsmpereira@gmail.com>
2013-06-13 12:13:45 +0100

    Run script: copy stats files to Dropbox.

--- run-daemon.sh
+++ run-daemon.sh
@@ -35,3 +35,6 @@ fi
 DATE=$(date +"%Y%m%d%H%M%S")
 gnome-terminal --tab -e "bash -c \"eval $func;exec bash\"" --tab --title "sar" -e "bash -c \"sar -n DEV,EDEV,UDP,TCP,ETCP 1 -o utils/$DATE.$file;exec bash\"" \
 --tab --title "routes" -e "bash -c \"while true; do clear; route -n; sleep 2; done\""
+
+echo "Copying stats to Dropbox ..."
+cp utils/2013* ~/Dropbox/TraxBot/chopin-routing-josepereira/
