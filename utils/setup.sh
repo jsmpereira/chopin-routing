f7bb568e566149a3179ec5dc94b4d8784584de66
Jose Santos <jsmpereira@gmail.com>
2013-07-05 16:01:06 +0100

    Update test options on setup.sh.

--- utils/setup.sh
+++ utils/setup.sh
@@ -64,7 +64,7 @@ function setup_iptables() {
     sudo sysctl -w net.ipv4.ip_forward=1
     echo "---> Setup iptables rules"
     echo "Select Topology:"
-    select top in "1" "2" "3.1" "3.2" "3.3" "5" "Back"; do
+    select top in "1" "2" "3.1" "3.2" "3.3" "4" "5" "Back"; do
 	case $top in
 	    Back ) break;;
 	    "3.2" | "3.3" ) update_test_topology $top;;
